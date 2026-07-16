52d688c7381b30d241e156fa3563b852f0ee262d
Alex Bourret <alexandre.bourret@dataiku.com>
2026-07-16 15:37:09 +0700

    adding control on paging size

--- python-lib/servicenow_commons.py
+++ python-lib/servicenow_commons.py
@@ -172,3 +172,10 @@ def get_parameters_from_config(config):
         sysparm_query = config.get("sysparm_query")
         sysparm_fields = config.get("sysparm_fields")
     return endpoint, display_values, sysparm_query, sysparm_fields
+
+
+def get_batch_size_from_config(config):
+    batch_size = None
+    if config.get("advanced_parameters", False) is True:
+        batch_size = config.get("page_size", None)
+    return batch_size
